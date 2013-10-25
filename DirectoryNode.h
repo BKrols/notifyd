6d0982f982b225e09e0bacc5c2c4ff8c6337fd12
Alexey Volkov <silakaselej@gmail.com>
2013-10-25 13:49:45 +0400

    Init new repo for project: "NotificationDaemon".

---
+++ DirectoryNode.h
@@ -0,0 +1,4 @@
+#ifndef _DIRECTORY_NODE_H_
+#define _DIRECTORY_NODE_H_
+
+#endif  /* _DIRECTORY_NODE_H_ */
---
+++ DirectoryTree.h
@@ -0,0 +1,4 @@
+#ifndef _DIRECTORY_TREE_H_
+#define _DIRECTORY_TREE_H_
+
+#endif  /* _DIRECTORY_TREE_H_ */
